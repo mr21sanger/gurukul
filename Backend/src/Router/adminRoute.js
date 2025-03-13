fce7636c7abc150e253ffd3cedf028594a252705
Shiwang Sanger <146845072+mr21sanger@users.noreply.github.com>
2025-03-13 22:02:08 +0530

    Update adminRoute.js

--- Backend/src/Router/adminRoute.js
+++ Backend/src/Router/adminRoute.js
@@ -10,7 +10,7 @@ let io;
 const initializeSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: "https://gurukul-learn.online/",
             methods: ["GET", "POST", "PUT", "DELETE"],
             credentials: true,
         },
